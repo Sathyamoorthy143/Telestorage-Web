d7296fcc14f77b00114cba6a0057169907e87239
Sathyamoorthy143 <sk143sathya@gmail.com>
2026-05-14 10:50:33 +0530

    Fix Vite outDir for Vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(async () => ({
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   clearScreen: false,
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
